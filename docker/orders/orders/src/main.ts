4b2e32ad4142eb7451aa5bbd5634c9442b3eba62
User Unknownn <abraaovictor000@gmail.com>
2023-09-25 14:57:31 -0300

    refactor(orders microservice): removing boilerplate

--- docker/orders/orders/src/main.ts
+++ docker/orders/orders/src/main.ts
@@ -1,10 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { OrdersModule } from './orders/orders.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(OrdersModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
